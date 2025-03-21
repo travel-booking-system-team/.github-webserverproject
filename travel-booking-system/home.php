1b6ca16c1d25c8e0b0115ec00d7190ac668a32e4
dptoala <158481267+dptoala@users.noreply.github.com>
2025-03-21 12:42:58 -0400

    Add files via upload

---
+++ travel-booking-system/home.php
@@ -0,0 +1,11 @@
+<?php 
+  include 'includes/sessions.php';
+  // Include header-member in the includes/ directory
+  include 'includes/header-member.php'; 
+?>
+
+<h1>Home</h1>
+<p><b>Incluide details of the home page<b></p>
+
+<!-- Include the footer in the includes/ directory -->
+<?php include 'includes/footer.php'; ?>
