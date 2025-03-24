2295eb64b2380b915b2f2f51045dd8cc9a7a4c79
Zenhayevsky <zenha.solorzano.acevedo@gmail.com>
2025-03-24 13:26:29 -0400

    aboutus and contacts adjustment
    fixed content in about us page. margin auto adjusted in contacts page.

--- travel-booking-system/pages/aboutus.php
+++ travel-booking-system/pages/aboutus.php
@@ -3,38 +3,9 @@
 <link rel="stylesheet" href="../css/main.css">
 
 <div class="register-container">
-    <h2>Sign Up</h2>
+    <h2>About us</h2>
 
-    <form action="process_register.php" method="POST">
-        <div class="form-group">
-            <label for="full_name">Full Name:</label>
-            <input type="text" id="full_name" name="full_name" required>
-        </div>
-
-        <div class="form-group">
-            <label for="email">Email:</label>
-            <input type="email" id="email" name="email" required>
-        </div>
-
-        <div class="form-group">
-            <label for="password">Password:</label>
-            <input type="password" id="password" name="password" required>
-        </div>
-
-        <div class="form-group">
-            <label for="confirm_password">Confirm Password:</label>
-            <input type="password" id="confirm_password" name="confirm_password" required>
-        </div>
-
-        <div class="form-buttons">
-            <button type="button" class="cancel-button">Cancel</button>
-            <button type="submit" class="submit-button">Sign Up</button>
-        </div>
-    </form>
-
-    <p class="login-link">
-        Already have an account? <a href="login.php">Log in here</a>
-    </p>
+    
 </div>
 
 <?php include '../includes/footer.php'; ?>
